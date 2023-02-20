831c13d8790c36632ef4c5e06e58132702137b81
Your Name <you@example.com>
2023-02-21 02:59:56 +0800

    change png

--- components/MySum.js
+++ components/MySum.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Text, View, StyleSheet, Image,Button,TextInput} from 'react-native';
 
 export default function MySum() {
-  const [value, onChangeText] = React.useState('Useless Placeholder');
+  const [value, onChangeText] = React.useState('');
   const [sum, setSum] = React.useState(0);
  // let sum=React.useState('0')
   return (
@@ -12,7 +12,7 @@ export default function MySum() {
        {sum}
       </Text>
        <TextInput
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        style={{ height: 60, width:200,borderColor: 'gray', borderWidth: 1 }}
         onChangeText={text => onChangeText(text)}
         value={value}
        />
